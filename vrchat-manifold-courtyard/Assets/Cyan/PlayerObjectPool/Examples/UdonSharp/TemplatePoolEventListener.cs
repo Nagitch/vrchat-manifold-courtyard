c62d24e586cc373164cd309ea051c79379c9afa5
kusanagi.y <kusanagi.y@mujina-prod.com>
2022-03-29 19:52:05 +0900

    import CyanPlayerObjectPool

---
+++ vrchat-manifold-courtyard/Assets/Cyan/PlayerObjectPool/Examples/UdonSharp/TemplatePoolEventListener.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using JetBrains.Annotations;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[AddComponentMenu("")]
+public class TemplatePoolEventListener : UdonSharpBehaviour
+{
+    // This event is called when the local player's pool object has been assigned.
+    [PublicAPI]
+    public void _OnLocalPlayerAssigned()
+    {
+        
+    }
+    
+    // This event is called when any player is assigned a pool object.
+    // The variables will be set before the event is called.
+    [PublicAPI, NonSerialized]
+    public VRCPlayerApi playerAssignedPlayer;
+    [PublicAPI, NonSerialized] 
+    public int playerAssignedIndex;
+    [PublicAPI, NonSerialized]
+    public UdonBehaviour playerAssignedPoolObject;
+    [PublicAPI]
+    public void _OnPlayerAssigned()
+    {
+        
+    }
+    
+    // This event is called when any player's object has been unassigned.
+    // The variables will be set before the event is called.
+    [PublicAPI, NonSerialized]
+    public VRCPlayerApi playerUnassignedPlayer;
+    [PublicAPI, NonSerialized] 
+    public int playerUnassignedIndex;
+    [PublicAPI, NonSerialized]
+    public UdonBehaviour playerUnassignedPoolObject;
+    [PublicAPI]
+    public void _OnPlayerUnassigned()
+    {
+        
+    }
+}
